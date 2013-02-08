9ce07be03d9e09375dd1fe269dc32fd6d4811a98
addisaden <addis.aden@gmail.com>
2013-02-08 09:51:29 +0100

    added userfile to parameterlist of new server

--- lib/cloudwars/server.rb
+++ lib/cloudwars/server.rb
@@ -1,16 +1,27 @@
-
+# encoding: utf-8
 
 module Cloudwars
   # The server is just the connectionpoint for cloudwars-clients.
-  # It has only two methods:
+  # It has only three methods:
   # - registration
   # - login
+  # - logout
   #
   # Here is all the Usermanagement and on successful login
   # it return an DRbObject named Player
   #
   class Server
-    def initialize(uri, safelevel=2)
+    # start the DRbFront
+    def initialize(uri, userfile=File.expand_path("../../userlist.yml", File.dirname(__FILE__)), safelevel=2)
+    end
+
+    def registration(user, password)
+    end
+
+    def login(user, password)
+    end
+
+    def logout(user, password)
     end
   end
 end
