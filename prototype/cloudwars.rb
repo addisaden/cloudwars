ede8d23e8fff4b9645795c09ea1d29654fc5af56
Adrian E <ae@writedown.eu>
2013-02-11 16:50:39 +0100

    prototype works

--- prototype/cloudwars.rb
+++ prototype/cloudwars.rb
@@ -23,7 +23,44 @@ module Cloudwars
 
       # return the playerinput
       def go(spielfeld, gegner)
-        Spielfeldwahl.new(0,0)
+        print_spielfeld(spielfeld, gegner)
+
+        puts "--- X : SPALTE --- Y : ZEILE ---"
+        print "#{ name.capitalize } ist am Zug (x y): ".color(color)
+        x, y = *gets.split.collect { |n| n.to_i }
+        return Spielfeldwahl.new(x,y)
+      end
+
+      def print_spielfeld(spielfeld, gegner)
+        puts "#{ name } : #{ towers }".bright.color( color )
+        puts "#{ gegner.name } : #{ gegner.towers }".color( gegner.color )
+
+        print "  "
+        spielfeld.count.times do |i|
+          print i%10 == 0 ? "#{ (i / 10).to_i } " : '  '
+        end
+        puts
+
+        print "  "
+        spielfeld.count.times do |i|
+          print "#{ i%10 } "
+        end
+        puts
+
+        spielfeld.each_with_index do |zeile, index_y|
+          print "%2s" % (index_y.to_s)
+          zeile.each do |feld|
+            if move * feld > 0 then
+              print (feld.abs == 4 ? 'X' : feld.abs).to_s.bright.color( color )
+            elsif gegner.move * feld > 0 then
+              print (feld.abs == 4 ? 'X' : feld.abs).to_s.color( gegner.color )
+            else
+              print ' '
+            end
+            print ' '
+          end
+          puts
+        end
       end
     end
 
@@ -35,13 +72,16 @@ module Cloudwars
 
         # Setup Spielfeldbreite
         loop do
-          print "Spielfeldbreite:" + " (8-32) ".color(:blue)
+          print "Spielfeldbreite:" + " (4-32) ".color(:blue)
           @spielfeldbreite = gets.strip.to_i
-          break if (8..32).include? @spielfeldbreite
-          puts "Ihre Angabe liegt nicht zwischen 8 und 32"
+          break if (4..32).include? @spielfeldbreite
+          puts "Ihre Angabe liegt nicht zwischen 4 und 32"
         end
 
-        @spielfeld = Array.new(@spielfeldbreite, Array.new(@spielfeldbreite, 0))
+        @spielfeld = []
+        @spielfeldbreite.times do
+          @spielfeld << Array.new(@spielfeldbreite, 0)
+        end
         @towers = ((@spielfeldbreite**2)/2 + 1).to_i
         @runde = 1
 
@@ -73,10 +113,14 @@ module Cloudwars
           # Spieler-Loop
           loop do
             puts "Runde: " + @runde.to_i.to_s.bright
+            puts "Towers: #{ @towers }".bright.color(:blue)
             player_move = player.go(@spielfeld, other(player))
-            case
+            case              
               when ! player_move.is_a?(Spielfeldwahl) then
                 puts "SYSTEM: Der Spieler muss eine Spielfeldwahl treffen".bright.color(:red)
+                next
+              when player_move.x.nil? || player_move.y.nil? then
+                puts "SYSTEM: Es wurde nur eine Zahl eingegeben"
                 next
               when ! (@spielfeld[player_move.y] && @spielfeld[player_move.y][player_move.x]) then
                 puts "SYSTEM: Das gewählte Spielfeld liegt ausserhalb der gültigen Felder, bitte wäheln Sie ein anderes".bright.color(:red)
@@ -92,12 +136,12 @@ module Cloudwars
 
             # set the move for player
             [-1, 0, 1].each do |y|
-              next if @spielfeld[y + py].nil?
+              next if @spielfeld[y + py].nil? or y+py < 0
               [-1, 0, 1].each do |x|
-                next if @spielfeld[y + py][x + px].nil?
+                next if @spielfeld[y + py][x + px].nil? or x+px < 0
 
                 # if the middle filed of the move do twice
-                ( x == 0 && y == 0 ? 2 : 1 ).times do
+                2.times do
                   # add one (direction == player.move = 1 / -1)
                   next unless (-3..3).include?( @spielfeld[y + py][x + px] )
                   @spielfeld[y + py][x + px] += player.move
@@ -106,7 +150,9 @@ module Cloudwars
                   if @spielfeld[y + py][x + px] == 4 * player.move && @towers > 0 then
                     player.add_tower
                     @towers -= 1
+                    break
                   end
+                  break if y != 0 or x != 0
                 end
               end
             end
@@ -114,8 +160,24 @@ module Cloudwars
           end
 
           # Testwinner and add half round
-          @runde += 0.5
-          break
+          if @towers > 0 && (@spieler_1.towers - @spieler_2.towers).abs <= @towers then
+            @runde += 0.5
+            next
+          else
+            break
+          end
+        end
+        if @spieler_1.towers == @spieler_2.towers then
+          puts "Es ist unentschieden.".bright.color(:blue)
+          @spieler_1.print_spielfeld(@spielfeld, @spieler_2)
+        elsif @spieler_1.towers > @spieler_2.towers then
+          puts "#{ @spieler_1.name.capitalize } hat gewonnen.".color( @spieler_1.color )
+          @spieler_1.print_spielfeld(@spielfeld, @spieler_2)
+        elsif @spieler_2.towers < @spieler_1.towers then
+          puts "#{ @spieler_2.name.capitalize } hat gewonnen.".color( @spieler_2.color )
+          @spieler_2.print_spielfeld(@spielfeld, @spieler_1)
+        else
+          puts "Das Spiel ist Zuende."
         end
       end
 
